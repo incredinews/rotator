bf4f1980758fde8c0c9969a44f074d848e810a00
User.Name <gist@github.com>
2024-09-23 16:19:58 +0200

    send pages,create dirs properly , index it ,show status

--- action.sh
+++ action.sh
@@ -2,9 +2,13 @@ echo "Loading"
 
 mkdir ./app;
 cp *.sh *.py ./app
-
+test -e cache && mv cache app/
+(
 cd ./app
 time bash run.sh
+for target in logs cache;do 
+test -e "$target" && mv $target ..
 
+)
 time sync
 exit 0
