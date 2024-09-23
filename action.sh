0701f092527885a8c950a127dc509a82e5e68791
User.Name <gist@github.com>
2024-09-23 17:49:24 +0200

    send pages,create dirs properly , index it ,show status

--- action.sh
+++ action.sh
@@ -2,11 +2,11 @@ echo "Loading"
 
 mkdir ./app;
 cp *.sh *.py ./app
-sed 's/HOST_NAME/'"${LOKI_HOST}"'/g' -i fluent.conf
-sed 's/USER_NAME/'"${LOKI_USER}"'/g' -i fluent.conf
-sed   's/API_KEY/'"${LOKI_TOKN}"'/g' -i fluent.conf
+sed 's/HOST_NAME/'"${LOKI_HOST}"'/g' -i fluent-conf/fluent-bit.conf
+sed 's/USER_NAME/'"${LOKI_USER}"'/g' -i fluent-conf/fluent-bit.conf
+sed   's/API_KEY/'"${LOKI_TOKN}"'/g' -i fluent-conf/fluent-bit.conf
 
-docker compose up -d 
+docker compose up &
 
 test -e cache && mv cache app/
 (
