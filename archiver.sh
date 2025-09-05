ab28add4a66d5a6e13395b614b20c2c7385fa941
sudo-bencho <182349246+sudo-bencho@users.noreply.github.com>
2025-09-05 20:42:05 +0000

    ....

--- archiver.sh
+++ archiver.sh
@@ -33,14 +33,14 @@ echo "found "$(echo "$hourslist"|wc -l)" hours"
 
 for myhour in $hourslist;do
 
-echo "Load $myhour"
-lftp -e "open ""$DAVURL""feedarchive/;mirror $myhour ;quit"
+echo $(date )" : Load $myhour"
+time lftp -e "open ""$DAVURL""feedarchive/;mirror -c --parallel=3 $myhour ;quit"
 ##links 
 export SENT_SOMETHING=false
 export RESTIC_REPOSITORY=/tmp/restic
 for feed in $(ls "$myhour" -1|cut -d_ -f1|sort -u );do  
 test -e /tmp/.del_$myhour || touch "/tmp/.del_$myhour"
-  for arch in $(ls "$myhour" -1|grep $feed);do 
+  for arch in $(ls "$myhour" -1|grep $feed|sort -n);do 
   ls -1 "$myhour/$arch" ;
   hostname=$(echo "$arch"|cut -d"_" -f1);
   timestamp=$(echo "$arch"|cut -d_ -f2-|cut -d"." -f1,2 |sed 's/_/ /g;s/\./:/g;s/$/:00/g');
@@ -48,8 +48,10 @@ test -e /tmp/.del_$myhour || touch "/tmp/.del_$myhour"
   links=$(cat "$myhour/$arch"|gunzip | tee "$myhour/"${arch/\.gz/} |jq .content|sed 's/\\n/\n/g'|grep "<link"|cut -d">" -f2|cut -d"<" -f1 )
   md5sum "$myhour/"${arch/\.gz/} 
   export SENT_SOMETHING=true
-  echo restic backup --time "$timestamp" --host "$hostname" "$myhour/"${arch/\.gz/} 
-       restic backup --time "$timestamp" --host "$hostname" "$myhour/"${arch/\.gz/} && echo "$myhour/$arch" >> "/tmp/.del_$myhour"
+  mkfifo /tmp/rst.io &>/dev/null|| true 
+  cat /tmp/rst.io |sed 's/^/'"$myhour"'| ADD:/g' &
+  #echo restic backup --time "$timestamp" --host "$hostname" "$myhour/"${arch/\.gz/} 
+       restic backup --time "$timestamp" --host "$hostname" "$myhour/"${arch/\.gz/} &> /tmp/rst.io  && ( echo "$myhour/$arch" >> "/tmp/.del_$myhour" )
   datestamp=$(date +%s -u -d "$timestamp")
   echo $timestamp " links: "$(echo "$links"|wc -l)
   ##                      batch n items for timestamp
@@ -57,7 +59,7 @@ test -e /tmp/.del_$myhour || touch "/tmp/.del_$myhour"
     tsout='"ts": {';out='{"urls": [';for m in $list;do out="$out"'"'"$m"'",';tsout="$tsout"'"'"$m"'": '"$datestamp"',' ;done  
     jsonout=$(echo "$out"|sed 's/,$/],/g')$(echo "$tsout"|sed 's/,$/}/g')"}"  ;
     #echo "$jsonout"|jq . -c ;#echo "$jsonout"
-    curl -s -H "API-KEY: $TSTOKEN" "${TSURL}" -H "Content-Type: application/json" -X POST  --data "${jsonout}"|jq .res.status|grep -v '":0'  -c ;echo
+    ( curl -s -H "API-KEY: $TSTOKEN" "${TSURL}" -H "Content-Type: application/json" -X POST  --data "${jsonout}"|jq .res|grep -v -e '": 0' -e '":0' |grep -v -e ^$ -e '^}$' -e '^{$' |sed 's/^/ADDURL:/g'   ;echo ) & sleep 0.5
   done
   test -e "$myhour/"${arch/\.gz/} && rm "$myhour/"${arch/\.gz/}
 
@@ -65,24 +67,28 @@ done;done
 [[ "$SENT_SOMETHING" = "true" ]] && { 
 restic forget --keep-hourly 2 --prune 2>&1|grep json |grep -v ^$|sed 's/^/'"$myhour"'| /g'
 
-mkfifo /tmp/rst.io || true 
+mkfifo /tmp/rst.io &>/dev/null|| true  
 
-[[ "$SECREADY" = "true" ]] && {  export RESTIC_REPOSITORY="$SECRESTURL";export AWS_SECRET_ACCESS_KEY="$SECRESTSKY";export AWS_ACCESS_KEY_ID="$SECRESTACK" ; restic copy --from-repo /tmp/restic &> /tmp/rst.log ;  cat /tmp/rst.log 2>&1|grep -v ^$|sed 's/^/'"$myhour"'| SEC:/g' ; restic forget --keep-hourly 2 --prune 2>&1|grep -v ^$|sed 's/^/'"$myhour"'| SEC:/g'  ;  } &
+[[ "$SECREADY" = "true" ]] && {  export RESTIC_REPOSITORY="$SECRESTURL";export AWS_SECRET_ACCESS_KEY="$SECRESTSKY";export AWS_ACCESS_KEY_ID="$SECRESTACK" ; restic copy --from-repo /tmp/restic &> /tmp/rst.log ;  cat /tmp/rst.log 2>&1|grep -v ^$|sed 's/^/'"$myhour"'| SEC:/g' ;   } &
 sleep 2
 export RESTIC_REPOSITORY="$RESTURL";export AWS_SECRET_ACCESS_KEY="$RESTSKY";export AWS_ACCESS_KEY_ID="$RESTACK"
 BACKUP_OK=false
-cat /tmp/rst.io |sed 's/^/'"$myhour"'| PRI:/g' 
+cat /tmp/rst.io |sed 's/^/'"$myhour"'| PRI:/g' &
 restic copy --from-repo /tmp/restic &> /tmp/rst.io && BACKUP_OK=true 
 wait
 
 
 [[ "$BACKUP_OK" = "true" ]] && ( cmdlist=$(cat "/tmp/.del_$myhour" |sed 's/^/ rm /g;s/$/;/g') ; echo "COPY OK.. delete source "$(echo "$cmdlist"|grep rm |wc -l ) ;echo "open ""$DAVURL""feedarchive/;""$cmdlist"";quit" |lftp & cat "/tmp/.del_$myhour" |while read a ;do test -e "$a" && rm "$a";done ;wait )   
+rm -rf /tmp/restic
+test -e /tmp/restic || mkdir /tmp/restic
+restic -r /tmp/restic init|| true
   echo -n ; } ;
 
-rm "/tmp/.del_$myhour"
+test -e "/tmp/.del_$myhour" && rm "/tmp/.del_$myhour"
+
 done
 
 export RESTIC_REPOSITORY="$RESTURL";export AWS_SECRET_ACCESS_KEY="$RESTSKY";export AWS_ACCESS_KEY_ID="$RESTACK"
-restic forget --keep-hourly 2 --prune 2>&1|grep -v ^$|sed 's/^/'"$myhour"'| PRI:/g'
+restic forget --keep-hourly 3 --prune 2>&1|grep -v ^$|sed 's/^/'"$myhour"'| PRI:/g'
 
-[[ "$SECREADY" = "true" ]] && {  export RESTIC_REPOSITORY="$SECRESTURL";export AWS_SECRET_ACCESS_KEY="$SECRESTSKY";export AWS_ACCESS_KEY_ID="$SECRESTACK" ; restic forget --keep-hourly 2 --prune 2>&1|grep -v ^$|sed 's/^/'"$myhour"'| SEC:/g'  ; } ; 
+[[ "$SECREADY" = "true" ]] && {  export RESTIC_REPOSITORY="$SECRESTURL";export AWS_SECRET_ACCESS_KEY="$SECRESTSKY";export AWS_ACCESS_KEY_ID="$SECRESTACK" ; restic forget --keep-hourly 3 --prune 2>&1|grep -v ^$|sed 's/^/'"$myhour"'| SEC:/g'  ; } ; 
