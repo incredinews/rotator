f7a76fea66311097da9311a38ee42f29c437e7c8
sudo-bencho <182349246+sudo-bencho@users.noreply.github.com>
2025-09-06 11:56:28 +0200

    Update archiver.sh

--- archiver.sh
+++ archiver.sh
@@ -36,6 +36,7 @@ echo "found "$(echo "$hourslist"|wc -l)" hours"
 for myhour in $hourslist;do
 
 echo $(date )" : Load $myhour"
+cd /tmp/feedarch
 time lftp -e "open ""$DAVURL""feedarchive/;mirror -c --parallel=3 $myhour ;quit"
 ##links 
 export SENT_SOMETHING=false
@@ -44,7 +45,8 @@ for feed in $(ls "$myhour" -1|cut -d_ -f1|sort -u );do
 lastsum=""
 lastcontentsum=""
 test -e /tmp/.del_$myhour || touch "/tmp/.del_$myhour"
-  for arch in $(ls "$myhour" -1|grep $feed|sort -n|grep gz);do 
+  for arch in $(ls "$myhour" -1|grep $feed|sort -n|grep gz);do
+  cd /tmp/feedarch
   echo  "                 "$(du -k "$myhour/$arch"|cut -d" " -f1)
   hostname=$(echo "$arch"|cut -d"_" -f1);
   timestamp=$(echo "$arch"|cut -d_ -f2-|cut -d"." -f1,2 |sed 's/_/ /g;s/\./:/g;s/$/:00/g');
