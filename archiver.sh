b32a2fafdddf5d9fd7499e9082395a47f5331768
sudo-bencho <182349246+sudo-bencho@users.noreply.github.com>
2025-09-06 10:24:40 +0000

    ..

--- archiver.sh
+++ archiver.sh
@@ -108,9 +108,9 @@ echo "$TSURL"|grep -e "^//::1" -e "//127\.0\.0\.1" && export BATCHSIZE=99
   lotsout='"ts": {';loout='{"urls": [';
   hitsout='"ts": {';hiout='{"urls": [';
   hictr=0
-  ( cat /tmp/urls.$feed/$m|grep -q -e ^http:// -e ^ftp:// -e ^redis:// -e ^rediss:// -e ^https:// -e ^dav:// -e ^davs:// -e ^smb:// -e ^s3:// ) && for elem in $sumlist;do 
   m=$(echo "$elem"|cut -d"/" -f2)
   feed=$(echo "$elem"|cut -d"/" -f1)
+  ( cat /tmp/urls.$feed/$m|grep -q -e ^http:// -e ^ftp:// -e ^redis:// -e ^rediss:// -e ^https:// -e ^dav:// -e ^davs:// -e ^smb:// -e ^s3:// ) && for elem in $sumlist;do 
   #echo $m
     loval=$(cat /tmp/seen.$feed/$m|sort -n |head -n1)
     hival=$(cat /tmp/seen.$feed/$m|sort -n |tail -n1)
